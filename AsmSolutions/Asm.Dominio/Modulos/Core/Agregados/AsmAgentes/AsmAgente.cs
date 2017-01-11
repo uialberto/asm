f9e08d5e49d95e5e50d5fc1b5b1580ae5046c458
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-01-11 11:04:38 -0400

    #3 Definicion entidad asmgente y frm apolo entities.

---
+++ AsmSolutions/Asm.Dominio/Modulos/Core/Agregados/AsmAgentes/AsmAgente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Asm.Dominio.Apolo.Entities;
+
+namespace Asm.Dominio.Modulos.Core.Agregados.AsmAgentes
+{
+    public class AsmAgente : Entity<Guid>
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
