5bd6e590f334efd1a2209d3df5d6c4fab2c276e0
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-02-13 22:39:10 -0400

    #6 Implementacion de inicializacion de automapper https://github.com/AutoMapper/AutoMapper/wiki/Getting-started

---
+++ AsmSolutions/Asm.Aplicacion/Helpers/AutoMapperConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Asm.Aplicacion.Helpers
+{
+    public static class AutoMapperConfiguration
+    {
+        public static void Initialize()
+        {
+            ConfigureBase();
+        }
+
+        private static void ConfigureBase()
+        {
+            Mapper.Initialize(config =>
+           {
+               config.CreateMap<Dominio.Modulos.Core.Agregados.Mascotas.Mascota, Dtos.DataTransfers.MascotaDto>()
+                   .ForMember(dto => dto.AsmAgenteId, opt => opt.MapFrom(entity => entity.AsmAgente.Id));
+
+               config.CreateMap<Dominio.Modulos.Core.Agregados.AsmAgentes.AsmAgente, Dtos.DataTransfers.AsmAgenteDto>()
+                   .ForMember(dto => dto.UserId, opt => opt.MapFrom(entity => entity.UserId));
+           });
+
+
+        }
+    }
+}
