753cd13ba3a108914a35fde071698f6de63d447b
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-02-14 00:26:11 -0400

    #7 Creacion de proyecto de prueba unitaria para proyecto de capa de aplicacion

---
+++ AsmSolutions/Asm.Aplicacion.Test/ApplicationServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Asm.Infra;
+using Asm.Infra.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Asm.Aplicacion.Test
+{
+    [TestClass]
+    public class ApplicationServiceTest
+    {
+        [AssemblyInitialize]
+        public static void TestAssemblyInitialize(TestContext context)
+        {
+
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Database.SetInitializer(new UnitOfWorkInitializer());
+            UnitOfWorkTestUtils.RestartUnitOfWork();
+            UnitOfWorkTestUtils.GetUnitOfWork().Database.Initialize(true);
+        }
+
+    }
+}
