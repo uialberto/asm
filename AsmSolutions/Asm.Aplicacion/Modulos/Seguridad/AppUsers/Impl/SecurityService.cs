7f3edaacea38b8796616b6c2b1255beb6c1a1674
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-02-13 23:14:39 -0400

    #6 Implementacion contenedor ioc con unity

---
+++ AsmSolutions/Asm.Aplicacion/Modulos/Seguridad/AppUsers/Impl/SecurityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asm.Aplicacion.Modulos.Seguridad.AppUsers.Impl
+{
+    public class SecurityService : ISecurityService
+    {
+    }
+}
