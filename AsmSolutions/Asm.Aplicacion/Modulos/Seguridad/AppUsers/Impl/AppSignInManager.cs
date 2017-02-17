cddab3feb70e4f3e7cca1524236755d5f7665abd
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-02-17 19:18:28 -0400

    Resolve #8 appsignin y appuser manager para seguridad con aspnet identity

---
+++ AsmSolutions/Asm.Aplicacion/Modulos/Seguridad/AppUsers/Impl/AppSignInManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Asm.Dominio.Modulos.Seguridad.Agregados.AppUsers;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+
+namespace Asm.Aplicacion.Modulos.Seguridad.AppUsers.Impl
+{
+    public class AppSignInManager : SignInManager<AppUser, string>
+    {
+        public AppSignInManager(AppUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public static AppSignInManager Create(IdentityFactoryOptions<AppSignInManager> options, IOwinContext context)
+        {
+            return new AppSignInManager(context.GetUserManager<AppUserManager>(), context.Authentication);
+        }
+
+        // ToDo Mas implementaciones.. 
+    }
+}
--- AsmSolutions/Asm.Aplicacion/Modulos/Seguridad/AppUsers/Impl/AppUserManager.cs
+++ AsmSolutions/Asm.Aplicacion/Modulos/Seguridad/AppUsers/Impl/AppUserManager.cs
@@ -22,8 +22,36 @@ namespace Asm.Aplicacion.Modulos.Seguridad.AppUsers.Impl
         public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
         {
             var manager = new AppUserManager(new UserStore<AppUser>(context.Get<UnitOfWork>()));
-            // ToDo Configuracion... 
+
+            #region Configuracion Validacion User y Password
+
+            manager.UserValidator = new UserValidator<AppUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 5,
+                RequireDigit = true,
+            };
+
+            #endregion
+
+            #region Configuracion de Bloqueo de Cuenta Inactiva
+
+            manager.UserLockoutEnabledByDefault = true;
+            manager.MaxFailedAccessAttemptsBeforeLockout = 6;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+            #endregion
+
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug it in here.
+            // Sample https://www.asp.net/identity/overview/features-api/two-factor-authentication-using-sms-and-email-with-aspnet-identity
+
             return manager;
+
         }
     }
 }
