a0cbe37c1ca68b48f0daff5e10db4e73b041c23b
Luis Alberto Baigorria Rodas <uialberto@gmail.com>
2017-02-17 18:32:42 -0400

    #8 creacion gestor appusermanager aspnet identity

---
+++ AsmSolutions/Asm.Aplicacion/Modulos/Seguridad/AppUsers/Impl/AppUserManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Asm.Dominio.Modulos.Seguridad.Agregados.AppUsers;
+using Asm.Infra;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace Asm.Aplicacion.Modulos.Seguridad.AppUsers.Impl
+{
+    public class AppUserManager : UserManager<AppUser>
+    {
+        public AppUserManager(IUserStore<AppUser> store)
+            : base(store)
+        {
+        }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
+        {
+            var manager = new AppUserManager(new UserStore<AppUser>(context.Get<UnitOfWork>()));
+            // ToDo Configuracion... 
+            return manager;
+        }
+    }
+}
